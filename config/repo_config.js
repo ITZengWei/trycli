1e0fd856bdeb6a917749b1b7037cc4f505bc75c2
曾威 <zengw02@mingyuanyun.com>
2022-02-10 15:17:19 +0800

    feat: 完成创建项目配置

---
+++ config/repo_config.js
@@ -0,0 +1,7 @@
+/** Vue仓库地址 注: 通过direct为url查找仓库.https://www.npmjs.com/package/download-git-repo#direct */
+const vueGitRepo = "direct:https://github.com/coderwhy/hy-vue-temp.git"
+
+module.exports = {
+  /** 王红元老师案例模板 */
+  why: vueGitRepo
+}
